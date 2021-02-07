91a7cd06cf3c156e1dc8c59844f625f4d4b1156b
tametes <72776921+tametes@users.noreply.github.com>
2021-02-06 20:04:40 -0500

    Update My_script.sh

--- Linux/My_script.sh
+++ Linux/My_script.sh
@@ -4,7 +4,7 @@ echo "hello $name."
 echo -e "\nThis is my script. \n"
 echo -e "The files in $PWD are: \n$(ls)"
 echo -e "\ncopying the passwd file to your current directory.\n"
-cp /etc/passwd $pwd
+cp /etc/passwd $PWD
 echo -e "the files in $pwd are now: \n$(ls)"
 echo ""
 echo "Have blessed day!"
