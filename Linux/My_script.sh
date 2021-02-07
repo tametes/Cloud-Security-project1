98d6bbd1eef085af3b5e33897ba1e5a560c0fe79
tametes <72776921+tametes@users.noreply.github.com>
2021-02-06 19:57:39 -0500

    Create My_script.sh

---
+++ Linux/My_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+name='jake'
+echo "hello $name."
+echo -e "\nThis is my script. \n"
+echo -e "The files in $PWD are: \n$(ls)"
+echo -e "\ncopying the passwd file to your current directory.\n"
+cp /etc/passwd $pwd
+echo -e "the files in $pwd are now: \n$(ls)"
+echo ""
+echo "Have blessed day!"
